7af60251344340a243bb7582952f3ba0473a2906
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2024-09-21 23:54:34 +0530

    Update MyBookRepository.java

--- src/main/java/com/bookStore/repository/MyBookRepository.java
+++ src/main/java/com/bookStore/repository/MyBookRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MyBookRepository extends JpaRepository<MyBookList,Integer> {
+public interface MyBookRepository extends JpaRepository<MyBookList,Integer> {
 }
